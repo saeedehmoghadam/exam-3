17afaddc5fa233b142384509eaa297e1b7e86f02
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-08-07 16:29:40 +0430

    Add files via upload

---
+++ adadrandom.py
@@ -0,0 +1,20 @@
+import random
+
+li=[]
+
+number = int(input("tole araye ra vared konid :"))
+
+for i in range (number):
+
+  m = random.randint(1,60)
+
+if li not in number:
+
+ m.append(li)
+
+ print(li)
+
+
+
+
+
