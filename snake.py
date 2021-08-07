12c7399334fbd9f2e821c39a8ff606943ccb82fb
saeedehmoghadam <88154810+saeedehmoghadam@users.noreply.github.com>
2021-08-07 16:29:14 +0430

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,20 @@
+import random
+
+
+length = int(input("type the snake length :"))
+
+for i in range(length):
+
+    if i % 2 == 0 :
+        print ("!",end ='')
+    else :
+        print("#",end ='')    
+
+
+
+
+
+
+ 
+
+
